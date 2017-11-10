d5f6e40fe2bd660e07dc76ed4ebb906ed4644312
Brecht Dierckx <brecht.dierckx@gmail.com>
2017-11-10 15:09:27 +0100

    ENS-1: - 'working' version of a naive NN with BE forecasting using MSE as loss. - Added first lines for probabilistic net a la Deep Ensemble paper

--- deep_ensemble/train_model.py
+++ deep_ensemble/train_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-from itertools import count
 
-import logging
 import torch
 import torch.autograd
 import torch.optim as optim
@@ -28,58 +26,106 @@ class ProbabilisticNet(nn.Module):
         super(ProbabilisticNet, self).__init__()
         self.inner_net = create_base_net(sizes+[2])
 
-    def forward(self, *input):
-        net = self.inner_net.forward(*input)
-        mean = net[0]
-        var = nn.Softplus(net[1]) + torch.autograd.Variable(torch.FloatTensor(1e-6))
-        return mean, var
+    def forward(self, input):
+        net = self.inner_net.forward(input)
+        mean = net[:, 0]
+        var = F.softplus(net[:, 1]) + torch.autograd.Variable(torch.ones(1)*1e-6)
+        return torch.t(torch.stack((mean, var)))
 
-    def nll(self, x, y):
-        mean, var = self(x)
-        a = torch.log(var)/2
-        b = 0.5/var
-
-        def loss_per_point(x1, y1):
-            return a - b*(y-mean)**2
-
-        return torch.sum([loss_per_point(x1,y1) for x1, y1 in zip(x,y)])
+    @staticmethod
+    def nll(x, y):
+        means = x[:, 0]
+        vars = x[:, 1]
+        a = torch.log(vars)/2
+        b = 0.5/vars
+        return torch.sum(a+b*torch.pow(y-means, 2))
 
 
-if __name__ == '__main__':
-    sizes = [1,20,20,1]
-    learning_rate = 1e-4
-    max_iter = 10000
-    batchsize=30
+def base_model(sizes):
     net = create_base_net(sizes)
-    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
     loss_fn = nn.MSELoss()
-    xs = np.expand_dims(np.linspace(-5, 5, num=100, dtype=np.float32), -1)
-    ts = np.cos(xs)
+    return loss_fn, net
+
+
+def proba_model(sizes):
+    net = ProbabilisticNet(sizes)
+    loss_fn = net.nll
+    return loss_fn, net
+
+
+def train_net(sizes, learning_rate, eps, alpha, max_iter, use_adv):
+    loss_fn, net = proba_model(sizes)
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    batch_size = 800
     losses = np.zeros(max_iter)
-    for i in range(0,max_iter):
-        indices = np.random.choice(np.arange(len(xs)), size=batchsize)
-        x = Variable(torch.from_numpy(xs[indices]))
-        t = Variable(torch.from_numpy(ts[indices]))
-        t_pred = net(x)
-        loss = loss_fn(t_pred, t)
-        losses[i] = loss.data[0]
+    for i in range(0, max_iter):
         optimizer.zero_grad()
+        indices = np.random.choice(np.arange(len(xs)), size=batch_size)
+        x = Variable(torch.from_numpy(xs[indices]+np.random.randn(batch_size)), requires_grad=True)
+        t = Variable(torch.from_numpy(ts[indices]))
+        pred = net(x)
+        loss = alpha*loss_fn(pred, t)
         # Backward pass: compute gradient of the loss with respect to model
         # parameters
-        loss.backward()
+        loss.backward(retain_graph=True)
+        if use_adv:
+            adv_sign = torch.sign(x.grad)
+            x_adv = x + eps * adv_sign
+            x_adv.volatile = False
+            pred_adv = net(x_adv)
+            loss_adv = (1-alpha)*loss_fn(pred_adv, t)
+            loss_adv.backward()
         # Calling the step function on an Optimizer makes an update to its
         # parameters
         optimizer.step()
-        print('iteration {} with loss {}'.format(i,loss.data[0]))
-        torch.save(net, './naive_trained_model.trch')
-    plt.figure()
-    plt.plot(losses)
+        joint_loss = loss.data[0] + loss_adv.data[0]
+        losses[i] += joint_loss
+        if i % 100 == 0:
+            print('iteration {} with loss {}'.format(i, joint_loss))
+    return net
 
-    plt.figure()
-    xs = np.expand_dims(np.linspace(-10, 10, num=100, dtype=np.float32), -1)
+
+def ensemble_mean_var(ensemble, x):
+    en_mean = np.zeros(len(x))
+    en_var = np.zeros(len(x))
+    for model in ensemble:
+        pred_ts = model(x)
+        mean = pred_ts[:, 0].data.numpy()
+        var = pred_ts[:, 1].data.numpy()
+        en_mean += mean
+        en_var += var + mean ** 2
+    en_mean /= len(ensemble)
+    en_var /= len(ensemble)
+    en_var -= en_mean ** 2
+    return en_mean, en_var
+
+
+if __name__ == '__main__':
+    xs = np.expand_dims(np.linspace(-5, 5, num=800, dtype=np.float32), -1)
     ts = np.cos(xs)
-    pred_ts = net(Variable(torch.from_numpy(xs)))
-    plt.plot(xs, pred_ts.data.numpy())
-    plt.plot(xs, ts)
-    plt.show()
+
+    learning_rate = 1e-3
+    sizes = [1, 16, 16, 1]
+    eps = 1e-2
+    alpha=0.5
+    max_iter = 800
+
+    # number of nets in the ensemble
+    K = 1
+
+    trained_nets = [train_net(sizes, learning_rate, eps, alpha, max_iter, use_adv=True) for _ in range(0,K)]
+    #plt.figure()
+    #plt.plot(losses)
+    if True:
+        plt.figure()
+
+        xs_np = np.expand_dims(np.linspace(-10, 10, num=100, dtype=np.float32), -1)
+        xs = Variable(torch.from_numpy(xs_np))
+        ts = np.cos(xs_np)
+
+        pred_ts = ensemble_mean_var(trained_nets, xs)
+        means, vars = pred_ts
+        plt.errorbar(x=xs_np.reshape(-1,1), y=means.reshape(-1,1), yerr=vars.reshape(-1,1))
+        plt.plot(xs_np.reshape(-1,1), ts.reshape(-1,1))
+        plt.show()
 
